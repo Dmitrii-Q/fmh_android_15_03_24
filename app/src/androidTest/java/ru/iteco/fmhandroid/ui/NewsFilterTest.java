2b0d4f8bf580b3a3f7e48e2a8a3d0d4b786808dd
Dmitrii <poltev.dmitrii.qa@gmail.com>
2024-12-22 18:14:50 +0300

    addAutoTests

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/NewsFilterTest.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/NewsFilterTest.java
@@ -1,10 +1,9 @@
 package ru.iteco.fmhandroid.ui;
 
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
@@ -13,12 +12,14 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import androidx.test.espresso.DataInteraction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -27,7 +28,6 @@ import androidx.test.filters.LargeTest;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,57 +36,77 @@ import ru.iteco.fmhandroid.R;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class TestAuthorization {
+public class NewsFilterTest {
 
     @Rule
     public ActivityScenarioRule<AppActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(AppActivity.class);
 
     @Test
-    public void testAuthorization() {
-        ViewInteraction textInputEditText = onView(
-                allOf(childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.login_text_input_layout),
-                                        0),
+    public void newsFilterTest() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.main_menu_image_button), withContentDescription("Main menu"),
+                        childAtPosition(
+                                allOf(withId(R.id.container_custom_app_bar_include_on_fragment_main),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
                                 0),
                         isDisplayed()));
-        textInputEditText.perform(click());
+        appCompatImageButton.perform(click());
 
-        ViewInteraction textInputEditText2 = onView(
-                allOf(childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.login_text_input_layout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textInputEditText2.perform(replaceText("login2"), closeSoftKeyboard());
-
-        ViewInteraction textInputEditText3 = onView(
-                allOf(childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.password_text_input_layout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textInputEditText3.perform(replaceText("password2"), closeSoftKeyboard());
-
-        ViewInteraction materialButton = onView(
-                allOf(withId(R.id.enter_button), withText("Sign in"), withContentDescription("Save"),
+        ViewInteraction materialTextView = onView(
+                allOf(withId(android.R.id.title), withText("News"),
                         childAtPosition(
                                 childAtPosition(
-                                        withClassName(is("android.widget.RelativeLayout")),
-                                        1),
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.filter_news_material_button),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.container_list_news_include),
+                                        0),
                                 2),
                         isDisplayed()));
         materialButton.perform(click());
 
-        ViewInteraction imageView = onView(
-                allOf(withId(R.id.trademark_image_view),
-                        withParent(allOf(withId(R.id.container_custom_app_bar_include_on_fragment_main),
-                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+        ViewInteraction checkableImageButton = onView(
+                allOf(withId(com.google.android.material.R.id.text_input_end_icon), withContentDescription("Show dropdown menu"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                0),
                         isDisplayed()));
-        imageView.check(matches(isDisplayed()));
+        checkableImageButton.perform(click());
+
+        DataInteraction materialTextView2 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(5);
+        materialTextView2.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.filter_button), withText("Filter"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                6),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction viewGroup = onView(
+                allOf(withParent(allOf(withId(R.id.news_item_material_card_view),
+                                withParent(withId(R.id.news_list_recycler_view)))),
+                        isDisplayed()));
+        viewGroup.check(matches(isDisplayed()));
     }
 
     private static Matcher<View> childAtPosition(
